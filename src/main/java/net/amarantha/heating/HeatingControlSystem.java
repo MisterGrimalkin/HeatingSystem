571f18a7ca2b067b09e456896b60510cefbcf7e2
MisterGrimalkin <loki@amarantha.net>
2015-07-28 19:22:02 +0100

    Fixed some things

--- src/main/java/net/amarantha/heating/HeatingControlSystem.java
+++ src/main/java/net/amarantha/heating/HeatingControlSystem.java
@@ -3,24 +3,13 @@ package net.amarantha.heating;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceServletContextListener;
-import com.google.inject.servlet.ServletModule;
-import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
-import com.sun.jersey.api.core.PackagesResourceConfig;
-import com.sun.jersey.api.core.ResourceConfig;
-import com.sun.jersey.core.spi.component.ioc.IoCComponentProviderFactory;
-import com.sun.jersey.guice.spi.container.GuiceComponentProviderFactory;
-import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 import net.amarantha.heating.hardware.HeatingController;
 import net.amarantha.heating.hardware.impl.MockHeatingController;
 import net.amarantha.heating.service.HeatingService;
 import net.amarantha.heating.utility.PropertyManager;
 import net.amarantha.heating.webservice.HeatingResource;
 
-import java.io.IOException;
-
-public class HeatingControlSystem extends GuiceServletContextListener {
+public class HeatingControlSystem {
 
     // Application
 
@@ -31,7 +20,7 @@ public class HeatingControlSystem extends GuiceServletContextListener {
     public void startApplication() {
 
         service.startHeatingService();
-        startWebService();
+        resource.startWebService();
 
         while ( true ) { }
 
@@ -50,37 +39,6 @@ public class HeatingControlSystem extends GuiceServletContextListener {
         Guice.createInjector(new Module())
                 .getInstance(HeatingControlSystem.class)
                         .startApplication();
-    }
-
-    public void startWebService() {
-        String url = "http://" + props.getString("ip", "127.0.0.1") + ":" + props.getString("port", "8001") + "/";
-        Injector injector = getInjector();
-//                server = GrizzlyHttpServerFactory.createHttpServer(url, rc, ioc);
-        ResourceConfig rc = new PackagesResourceConfig( "net.amarantha.heating.webservice" );
-        IoCComponentProviderFactory ioc = new GuiceComponentProviderFactory( rc, injector );
-//        ResourceConfig rc = new ResourceConfig().packages("net.amarantha.heating.webservice");
-        try {
-            GrizzlyServerFactory.createHttpServer(url, rc, ioc).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//        serve( "/heating/*" ).with( GuiceContainer.class );
-    }
-
-    @Override
-    protected Injector getInjector() {
-        return Guice.createInjector(
-                new ServletModule() {
-                    @Override
-                    protected void configureServlets() {
-                        ResourceConfig rc = new PackagesResourceConfig("net.amarantha.heating");
-                        for (Class<?> resource : rc.getClasses()) {
-                            bind(resource);
-                        }
-                        serve("/heating/*").with(GuiceContainer.class);
-                    }
-                }
-        );
     }
 
 }
--- src/main/java/net/amarantha/heating/webservice/HeatingResource.java
+++ src/main/java/net/amarantha/heating/webservice/HeatingResource.java
@@ -1,117 +1,126 @@
 package net.amarantha.heating.webservice;
 
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import net.amarantha.heating.service.HeatingService;
 import net.amarantha.heating.utility.PropertyManager;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 
 import static net.amarantha.heating.entity.Status.OFF;
 import static net.amarantha.heating.entity.Status.ON;
 
-@Singleton
 @Path("heating")
+@Singleton
 public class HeatingResource {
 
-    @Inject
-    protected HeatingService service;
+    protected static HeatingService service;
 
     @Inject
     protected PropertyManager props;
 
+    @Inject
+    public HeatingResource(HeatingService service) {
+        HeatingResource.service = service;
+    }
+
+    public void startWebService() {
+        ResourceConfig rc = new ResourceConfig().packages("net.amarantha.heating.webservice");
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(props.getString("ip","127.0.0.1")), rc);
+    }
 
     @GET
     @Path("status")
-    public Response getStatus() {
+    public static Response getStatus() {
+        // TODO: JSON
         return ok("Not Implemented");
     }
 
     @GET
     @Path("timer-events")
-    public Response getTimerEvents() {
+    public static Response getTimerEvents() {
+        // TODO: JSON
         return ok("Not Implemented");
     }
 
     @POST
     @Path("on")
-    public Response switchHeatingOn() {
+    public static Response switchHeatingOn() {
         service.switchHeating(ON);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("off")
-    public Response switchHeatingOff() {
+    public static Response switchHeatingOff() {
         service.switchHeating(OFF);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("override/on")
-    public Response overrideOn() {
+    public static Response overrideOn() {
         service.setOverride(ON);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("override/off")
-    public Response overrideOff() {
+    public static Response overrideOff() {
         service.setOverride(OFF);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("thermo/on")
-    public Response thermoOn() {
+    public static Response thermoOn() {
         service.setThermo(ON);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("thermo/off")
-    public Response thermoOff() {
+    public static Response thermoOff() {
         service.setThermo(OFF);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("timer/on")
-    public Response timerOn() {
+    public static Response timerOn() {
         service.setTimer(ON);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("timer/off")
-    public Response timerOff() {
+    public static Response timerOff() {
         service.setTimer(OFF);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("timer/remove")
-    public Response removeTimerEvent(@QueryParam("id") String id) {
+    public static Response removeTimerEvent(@QueryParam("id") String id) {
         service.removeTimerEvent(id);
-        return ok();
+        return ok("Command Processed");
     }
 
     @POST
     @Path("timer/add")
-    public Response addTimerEvent(@QueryParam("type") String type, @QueryParam("time") String time) {
-        String id = service.addTimerEvent("ON".equals(type) ? ON : OFF, time);
+    public static Response addTimerEvent(@QueryParam("type") String type, @QueryParam("time") String time) {
+        String id = service.addTimerEvent("ON".equals(type)?ON:OFF,time);
         return ok(id);
     }
 
-    private Response ok() {
-        return ok("Request Processed");
-    }
-
-    private Response ok(String content) {
+    private static Response ok(String content) {
         return Response.ok()
                 .header("Access-Control-Allow-Origin", "*")
                 .entity(content)
